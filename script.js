742fab7aae18392051c8281d77caf50705530da6
Simon West <43855704+simonwestuk@users.noreply.github.com>
2021-03-17 10:23:31 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function openSide()
+{
+    var x = document.getElementById('side');
+
+    x.style.width = '15vw';
+}
+
+function closeSide()
+{
+    var x = document.getElementById('side');
+
+    x.style.width = '0vw';
+}
